44e4e1f271101961c550c21816d967b619ecd41a
Dylan Carver <dylancarver14@gmail.com>
2023-05-21 17:08:55 -0500

    learned dependency injection

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core"; // import declerations
-// import { CoursesService } from "./courses.service";
+import { CoursesService } from "./courses.service";
 
 @Component({ // making custom HTML elements
     selector: 'courses',
-    template: `<h2>{{ title }}<h2>
+    template: `
+    <h2>{{ title }}<h2>
     <ul>
         <li *ngFor="let course of courses">
             {{ course }}
@@ -14,6 +15,10 @@ import { Component } from "@angular/core"; // import declerations
 export class CoursesComponent {
     title = "List of courses"
     courses
+
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses()
+    }
 
     // Can be called by invoking the fuction in the template
     // getTitle() {
