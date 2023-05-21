2e127fcb5d99b9883e1ce3f9aa2575763a3ed594
Dylan Carver <dylancarver14@gmail.com>
2023-05-21 11:45:52 -0500

    print to the terminal

---
+++ typescript-hello/main.ts
@@ -0,0 +1,14 @@
+// Anything written is typescript get transpiled to javascript
+// when you run tsc {file name that is a typescript file}
+
+// Then you can run the .js file using node {transpiled .js file}
+
+// function log(message) {
+//     console.log(message)
+// }
+
+// let message = "Hello World"
+
+// log(message)
+
+// ***************************************************************
