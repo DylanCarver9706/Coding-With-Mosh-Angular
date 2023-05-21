84c8df1bc956454bb2ddf039ef61b8a82088990a
Dylan Carver <dylancarver14@gmail.com>
2023-05-21 15:13:12 -0500

    learn the difference between interface and class

---
+++ typescript-classes/main.ts
@@ -0,0 +1,13 @@
+class Point {
+    x: number; // field used for storing data
+    y: number;
+    
+    draw() { //method
+        // ...
+    }
+
+    getDistance(another: Point) {
+        // ...
+    }
+}
+
