3aa5c3aa7d816780dd75d0dbc391b19931005da0
Dylan Carver <dylancarver14@gmail.com>
2023-05-21 16:51:22 -0500

    learn how to dynamically render info to page

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,12 +2,20 @@ import { Component } from "@angular/core"; // import declerations
 
 @Component({ // making custom HTML elements
     selector: 'courses',
-    template: '<h2>{{ "Title: " + getTitle() }}<h2>' 
+    template: `<h2>{{ title }}<h2>
+    <ul>
+        <li *ngFor="let course of courses">
+            {{ course }}
+        </li> 
+    <ul>
+    `
 })
 export class CoursesComponent {
     title = "List of courses"
+    courses = ["course1", "course2", "course3"]
 
-    getTitle() {
-        return this.title;
-    }
+    // Can be called by invoking the fuction in the template
+    // getTitle() {
+    //     return this.title;
+    // }
 }
