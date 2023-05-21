a8d096e47ce63c51b81ed6169bf0d1c92b671bc6
Dylan Carver <dylancarver14@gmail.com>
2023-05-21 14:56:55 -0500

    learn different ways to write a function

---
+++ typescript-arrow-functions/main.ts
@@ -0,0 +1,11 @@
+// Ways to write functions
+
+let log = function(message) {
+    console.log(message);
+}
+
+let twoLineLog = (message) => {
+    console.log(message);
+}
+
+let oneLineLog = (message) => console.log(message);
