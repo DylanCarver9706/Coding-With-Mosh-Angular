3995d7cb1501ceb4fe39c0903c2521c0ffce4b5b
Dylan Carver <dylancarver14@gmail.com>
2023-05-21 16:45:51 -0500

    learn how to generate components

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,8 +2,12 @@ import { Component } from "@angular/core"; // import declerations
 
 @Component({ // making custom HTML elements
     selector: 'courses',
-    template: '<h2>Courses<h2>' 
+    template: '<h2>{{ "Title: " + getTitle() }}<h2>' 
 })
 export class CoursesComponent {
-    
+    title = "List of courses"
+
+    getTitle() {
+        return this.title;
+    }
 }
